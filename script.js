45c4d8a0385e560d370e79022bdd1c490b192e08
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-29 14:01:48 +0530

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.getElementById('integralForm').addEventListener('submit', function(event) {
     event.preventDefault();  
 
-    //get value of both a
+    //get value of both a a
     const func = document.getElementById('function').value.trim();
     const a = parseFloat(document.getElementById('lowerBound').value);
     const b = parseFloat(document.getElementById('upperBound').value);
