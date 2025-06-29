ae25639e140024cb8d49a379affc4164c3d32ba8
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-29 14:17:07 +0530

    script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.getElementById('integralForm').addEventListener('submit', function(even
       return;
     }
   
-   //get result
+   //get result o
     const result = calculateDefiniteIntegral(func, a, b);
   
 
